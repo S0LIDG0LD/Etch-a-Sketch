35c406fa06d74893783866d20848b4dd2c970f80
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-08-04 17:11:16 +0100

    Added a button on the top of the screen to send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before so that you've got a new sketch pad. The limit for the user input is maximum 100.

--- javascript.js
+++ javascript.js
@@ -1,35 +1,43 @@
-const ROWS_NUM = 16;
-const COLS_NUM = 16;
+let rowsColsNum = 16;
 
-const div = document.querySelector("div");
+const container = document.querySelector("#container");
 
 function createGrid() {
-    for (i = 1; i < ROWS_NUM + 1; i++) {
 
-        for (j = 1; j < COLS_NUM + 1; j++) {
-
+    for (i = 1; i < rowsColsNum + 1; i++) {
+        
+        for (j = 1; j < rowsColsNum + 1; j++) {
+        
             const innerDiv = document.createElement("div");
             innerDiv.classList.toggle(`innerDiv`);
             innerDiv.classList.toggle(`row${i}`);
             innerDiv.classList.toggle(`col${j}`);
             innerDiv.setAttribute("id", `divR${i}C${j}`);
+            let size = 100 / rowsColsNum;
+            innerDiv.style.flexBasis = size + "%";
             innerDiv.addEventListener('mouseenter', () => {
                 innerDiv.classList.add('hoveredDiv');
             });
-            // innerDiv.addEventListener('mouseleave', () => {
-            //     innerDiv.classList.remove('hoveredDiv');
-            // });
-            div.appendChild(innerDiv);
+            container.appendChild(innerDiv);
         } 
     }
 }
 
+
+
+const newSketchpad = document.querySelector(".new");
+newSketchpad.addEventListener('click', () => {
+    rowsColsNum = 0;
+    do
+    {
+        rowsColsNum = Number(prompt("Insert grid size nXn, n(max 100): "));
+    }
+    while (rowsColsNum < 1 || rowsColsNum > 100);
+    let innerDivs = document.querySelectorAll(".innerDiv");
+    innerDivs.forEach(innerDiv => {
+        container.removeChild(innerDiv);
+    })
+    createGrid();
+});
+
 createGrid();
-//     const paperButton = document.createElement("button");
-// paperButton.setAttribute("id", "Paper");
-// paperButton.classList.toggle("playButton");
-// paperButton.style.margin = "10px";
-// paperButton.textContent = "Paper"
-// paperButton.addEventListener('click', () => {
-//         playRound(paperButton.id, getComputerChoice());
-//     });
