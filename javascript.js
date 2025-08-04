0ff0d68fbef9fc5013a1404390f19e99ded9b83f
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-08-04 15:42:37 +0100

    1. Created 16x16 divs flexbox using JavaScript. 2. Put the grid squares inside a “grid” div. 3. Used Flexbox to make the divs appear as a grid. 4. Added some borders, rounded corners, padding and margins.

---
+++ javascript.js
@@ -0,0 +1,29 @@
+const ROWS_NUM = 16;
+const COLS_NUM = 16;
+
+const div = document.querySelector("div");
+
+function createGrid() {
+    for (i = 1; i < ROWS_NUM + 1; i++) {
+
+        for (j = 1; j < COLS_NUM + 1; j++) {
+
+            const innerDiv = document.createElement("div");
+            innerDiv.classList.toggle(`innerDiv`);
+            innerDiv.classList.toggle(`row${i}`);
+            innerDiv.classList.toggle(`col${j}`);
+            innerDiv.setAttribute("id", `divR${i}C${j}`);
+            div.appendChild(innerDiv);
+        } 
+    }
+}
+
+createGrid();
+//     const paperButton = document.createElement("button");
+// paperButton.setAttribute("id", "Paper");
+// paperButton.classList.toggle("playButton");
+// paperButton.style.margin = "10px";
+// paperButton.textContent = "Paper"
+// paperButton.addEventListener('click', () => {
+//         playRound(paperButton.id, getComputerChoice());
+//     });
